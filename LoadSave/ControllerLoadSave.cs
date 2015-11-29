a95047e0f8dd760d2d1a3e3a6b587deaab7c79f9
Martin Obel Thomsen <obelth@gmail.com>
2015-11-29 16:39:00 +0100

    Tilføjet dll

--- LoadSave/ControllerLoadSave.cs
+++ LoadSave/ControllerLoadSave.cs
@@ -1,8 +1,4 @@
-﻿using TestApp;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
